162ee0e38fc716f2f1fce8613ce0229e4e08bd72
Robert L. Read <read.robert@gmail.com>
2015-05-02 15:30:59 -0500

    Update Artificial_Infant.ino

--- incubator/Artificial_Infant.ino
+++ incubator/Artificial_Infant.ino
@@ -8,6 +8,15 @@
 // GREEN means just right baby
 // Right now if you want to change the window you have to reprogramm it digitally 
 // by changing these value.
+
+
+// I'm using this to save power.  http://www.reddit.com/r/arduino/comments/o5443/so_you_want_an_easy_way_to_save_power/
+// On a standard Arduino R3, this won't save much power, but it gets us ready for using a better board in the actual
+// application....and the more experience we get with that the better.  The article mentions Arduino-compatible
+// non-prototype solutions that may be much superior to Arduino R3s, which are great for prototyping.
+#include <avr/io.h>
+
+
  const float MAX_TEMP = 26.0;
  const float MIN_TEMP = 24.0;
  
@@ -46,9 +55,6 @@ void loop()                     // run over and over again
  temperature = (temperature - .5) * 100;          //converting from 10 mv per degree wit 500 mV offset
                                                   //to degrees ((volatge - 500mV) times 100)
  Serial.println(temperature);                     //printing the result
- /* Example - 2 Go through Random Colors
-  Set the LEDs to a random color
-*/
    if (temperature < MIN_TEMP) {
      setColor(ledDigitalOne,BLUE);
    } else if (temperature > MAX_TEMP) {
@@ -56,9 +62,13 @@ void loop()                     // run over and over again
    } else {
      setColor(ledDigitalOne,GREEN);
    }
-   
- delay(1000);                                     //waiting a second
-
+  // Let's flash the LED long enough for people to see it, then go to sleep to save power....
+  // Note: If 
+// lpDelay(1);
+ delay(250);
+ setColor(ledDigitalOne,BLACK);
+//  lpDelay(7);
+ delay(1750);
 
 }
 // this code copied from ARDX (thanks!)
@@ -80,13 +90,6 @@ void setup(){
                        //to view the result open the serial monitor 
                        //last button beneath the file bar (looks like a box with an antenae)
 }
-// We're not using this!
-
-void randomColor(){
-  int rand = random(0, sizeof(COLORS) / 2);  //get a random number within the range of colors
-  setColor(ledDigitalOne, COLORS[rand]);  //Set the color of led one to a random color
-  delay(1000);
-}
 
 /* Sets an led to any color
    led - a three element array defining the three color pins (led[0] = redPin, led[1] = greenPin, led[2] = bluePin)
@@ -104,3 +107,18 @@ void setColor(int* led, const boolean* color){
   boolean tempColor[] = {color[0], color[1], color[2]};
   setColor(led, tempColor);
 }
+
+
+/*
+  lpDelay(quarterSeconds) - Low Power Delay.  Drops the system clock
+                            to its lowest setting and sleeps for 256*quarterSeconds milliseconds.
+*/
+int lpDelay(int quarterSeconds) {
+  int oldClkPr = CLKPR;  // save old system clock prescale
+  CLKPR = 0x80;    // Tell the AtMega we want to change the system clock
+  CLKPR = 0x08;    // 1/256 prescaler = 60KHz for a 16MHz crystal
+  delay(quarterSeconds);  // since the clock is slowed way down, delay(n) now acts like delay(n*256)
+  CLKPR = 0x80;    // Tell the AtMega we want to change the system clock
+  CLKPR = oldClkPr;    // Restore old system clock prescale
+}
+
