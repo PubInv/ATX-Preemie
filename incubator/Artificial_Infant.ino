9982a80c0c36829d7e0c55900d1ea2e04993869e
Robert L. Read <read.robert@gmail.com>
2015-06-09 21:52:53 -0500

    adding license

--- incubator/Artificial_Infant.ino
+++ incubator/Artificial_Infant.ino
@@ -1,4 +1,17 @@
-// This code placed in the public domain by Engineers Without Borders, Austin Chapter.
+// Copyright Robert L. Read, <read.robert@gmail.com>.
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // The the temperatures here for a "cold baby" (MIN_TEMP) and "hot baby" (MAX_TEMP).
 // If the RGB LED is properly configured on pins 9,10, and 11, and the TMP36 sensor 
